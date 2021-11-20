3798e6e6e81d035cb2ccdb84ca626993c797591e
Baran Yılmaz Yücel <baranyucel643@gmail.com>
2021-11-20 18:41:34 +0300

    Update cmtozurna.py
    Kodlar basitleştirildi

--- cmtozurna.py
+++ cmtozurna.py
@@ -4,28 +4,24 @@
 import os
 import time 
 cls = lambda: os.system('cls')
-#
 
 #For input/Girdiyi almak için
-print("Choose a language/Bir dil seç! (TR or ENG) ")
+print("Choose a language/Bir dil seç! (TR or ENG)")
 lang = input().lower()
 
 time.sleep(0.2)
+cls()
 
 #For Turkish/Türkçe için
 if lang == "tr":
-    print("Santimetreden Zurnaya Dönüştürme Aracı")
-    print(" ")
-    cm = int(input("Santimetre giriniz: "))
-    print(" ")
+    print("Santimetreden Zurnaya Dönüştürme Aracı \n")
+    cm = int(input("Santimetre giriniz: \n"))
     print("Sonuç:", cm / 70)
 
 #For English/İngilizce için
 elif lang == "eng":
-    print("Centimeter to Zurna")
-    print(" ")
-    cm = int(input("Enter Centimeter: "))
-    print("")
+    print("Centimeter to Zurna \n")
+    cm = int(input("Enter Centimeter: \n"))
     print("Result:", cm / 70)
 
 #End of code
